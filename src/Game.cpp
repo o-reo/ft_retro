ce49f617a32f2f7983109708a17e35f91194e03a
o-reo <em.ruaud@gmail.com>
2020-01-18 19:05:14 +0100

    Fixed leak

--- src/Game.cpp
+++ src/Game.cpp
@@ -21,12 +21,13 @@ Game::Game() : entities(nullptr), end(false) {
 }
 
 Game::~Game() {
+  Logger *log = Logger::get();
   Game::EntityNode *node = this->entities;
   while (node) {
     node = this->destroyEntityNode(node);
+    node = node->next;
   }
   endwin();
-  Logger *log = Logger::get();
   log->out() << "Closing window" << std::endl;
   delete log;
   this->end = true;
@@ -57,9 +58,9 @@ void Game::checkCollisions() {
 }
 
 Game::EntityNode *Game::destroyEntityNode(EntityNode *node) {
+  Game::EntityNode *pnode = this->entities;
   if (node == this->entities)
     this->entities = node->next;
-  Game::EntityNode *pnode = this->entities;
   while (pnode && pnode->next) {
     if (pnode->next == node) {
       pnode->next = node->next;
